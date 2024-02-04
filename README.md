a48c2d499b95db6ca6541efe67bc61af06a1e18c
Oleksandr Smolin <smoolrich@gmail.com>
2024-02-04 19:55:32 +0100

    Add lazy loading to avoid credentialsProperties bean loading in integration tests

--- README.md
+++ README.md
@@ -3,3 +3,8 @@ and then you can use `software.virtualline.hashicorpautoconfig.properties.Creden
 passwords which are stored on HashiCorp service
 
 
+How to publish it correctly:
+
+1. **change** library version in build.gradle file: `org.gradle.api.Project#setVersion` 
+2. push your changes to repository
+3. run custom gradle command: `./gradlew publishing`
--- src/main/java/software/virtualline/hashicorpautoconfig/config/LibraryConfig.java
+++ src/main/java/software/virtualline/hashicorpautoconfig/config/LibraryConfig.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import software.virtualline.hashicorpautoconfig.api.HashiCorpApi;
 import software.virtualline.hashicorpautoconfig.properties.CredentialsProperties;
 
@@ -15,6 +16,7 @@ public class LibraryConfig {
     private final HashiCorpApi hashiCorpApi;
 
     @Bean
+    @Lazy
     public CredentialsProperties credentialsProperties() {
         return new CredentialsProperties(hashiCorpApi.fetchPassword());
     }
